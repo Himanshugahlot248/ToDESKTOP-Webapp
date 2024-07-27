e8916dbe1d331a7b3a24180c0a8df8e3b4ecca30
Himanshu Gahlot <117620675+Himanshugahlot248@users.noreply.github.com>
2024-07-27 18:57:26 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'display': ['Poppins', 'sans-serif'],
+        'body': ['Inter', 'sans-serif']
+      },
+      colors:{
+         'my':['dodgerblue']
+      },
+      keyframes: {
+        slidein: {
+          from: {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        slidein: "slidein 1s ease 300ms",
+      },
+
+    },
+  },
+  plugins: [],
+};
